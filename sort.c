bd76233df83994ed2a4c2d786f2785ab88da5f35
kianamcdaniel <40674230+kianamcdaniel@users.noreply.github.com>
2018-09-22 08:25:18 -0500

    Update sort.c

--- sort.c
+++ sort.c
@@ -38,7 +38,7 @@ int main(){
         }
     }
     
-    for(int i = 0; i < count; i++){
+    for(int i = 1; i < count; i++){
         for(int j = 0; j < 2; j++){
             printf("%c", name[i][j]);
         }
