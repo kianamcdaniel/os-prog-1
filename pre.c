d477281aa6ac61176b5a62e76504b41d03c027f9
kianamcdaniel <40674230+kianamcdaniel@users.noreply.github.com>
2018-09-24 13:02:47 -0500

    Update pre.c

--- pre.c
+++ pre.c
@@ -18,25 +18,17 @@ int main()
 {
     struct state arr_states[10];
     int count = 0;
-    
-    //printf("\nEnter a state and it's populations (in millions) as shown below\n\n");
-    //printf("TX 666   where 'TX' is the state's abbreviation and '666' is in millions\n\n");
-    //printf("To enter another state, simply press the 'enter' button.\nWhen you are done entering the list of states, press 0\n\n");
-	
+    	
     while(scanf("%s", arr_states[count].name)
           && (strcmp(arr_states[count].name, "0") != 0) && count < 10){
         scanf("%d", &arr_states[count].population);
         count++;
     }
-    
-    //printf("%d", count);
-    //printf("\n\nHere are the states whose population are greater than 10 million:\n\n");
-	
-    for(int i = 0; i < count; i++)
-	{
+    	
+    for(int i = 0; i < count; i++){
         if (arr_states[i].population > 10){
             printf("%s \n", arr_states[i].name);
         }
-	}
-	printf("0");
+    }
+    printf("0");
 }
