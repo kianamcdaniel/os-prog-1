8b5c091dc1978f9701fcdd257ee8032c2a6b9db3
kianamcdaniel <40674230+kianamcdaniel@users.noreply.github.com>
2018-09-24 17:07:52 -0500

    Update ls.c

--- ls.c
+++ ls.c
@@ -11,26 +11,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-struct command{
-    char *name;
-    int argc;
-    char *argv[];
-};
-
 int main (int argc, char *argv[]){
-    struct command *command;
+    char *command;
     int p[2];
-    int flag = 1;
+    int count = 0;
     pid_t pid;
+    pipe(p);
     
-    printf("Enter the command 'ls -t -l': ");
-    scanf("%s %s %s", command->name, command->argv[1], command->argv[2]);
-    printf("%s %s %s", command->name, command->argv[1], command->argv[2]);
+    pid = fork();
     
-    /*pid = fork();
-    
-    if(pipe(p) == -1){
-        perror("pipe call");
+    if(pid < 0){
+        printf("ERROR");
         exit(1);
-    }*/
+    }
+    else if(pid == 0){
+        execvp(argv[1], &argv[1]);
+        perror("Error: ");
+        exit(1);
+    }
+    else{
+        wait(NULL);
+        printf("Child has terminated\n");
+        printf("Child PID: %d\n", getpid());
+        printf("Parent PID: %d\n", getppid());
+    }
+    
 }
